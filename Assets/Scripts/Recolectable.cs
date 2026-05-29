1431c45409a7d16de493a069140458d3fdae68b1
Joaquinsanchezlaffont <50159309@est.ort.edu.ar>
2026-05-29 10:08:36 -0300

    clase2

---
+++ Assets/Scripts/Recolectable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recolectable : MonoBehaviour
+{
+    public static int puntos = 0;
+
+    void OnTriggerEnter(Collider otro)
+    {
+        if (otro.gameObject.CompareTag("Player"))
+        {
+            puntos = puntos + 1;
+
+            Debug.Log("Objeto recolectado. Puntos: " + puntos);
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/UIpuntos.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIpuntos : MonoBehaviour
+{
+    public Text textoPuntos;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        textoPuntos.text = "Puntos: " + Recolectable.puntos;
+    }
+}
